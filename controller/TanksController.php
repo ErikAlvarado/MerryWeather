987875f58e639dd53db1df80412913516d5b5bb1
Erik Alvarado <erikalva11g@gmail.com>
2026-03-04 17:11:05 -0600

    Error en TanksController.php

--- controller/TanksController.php
+++ controller/TanksController.php
@@ -1,6 +1,6 @@
 <?php
 // Usamos __DIR__ para asegurar que encuentre los archivos sin importar desde dónde se llame el controlador
-require_once __DIR__ . "/../config/Database.php";
+require_once __DIR__ . "/../config/DataBase.php";
 require_once __DIR__ . "/../models/Tanks.php";
 
 class TanksController {
